ef7399370c23e98eec13fe6ae12ac5c0a915f387
Kris Lai <krislai@test.com>
2019-08-13 11:06:18 -0400

    Curl json file

--- test.sh
+++ test.sh
@@ -1,8 +1,17 @@
-# This will push message to Kris
+# This will push message to Receiver
 
 # Take first argument from input
-message=$1
+receiver=$1
+message=$2
 
-# Sends message to Kris
-curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"${message}\"}" https://hooks.slack.com/services/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
+if [ $receiver = "j" ]; then
+  curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"${message}\"}" https://hooks.slack.com/services/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
+elif [ $receiver = "k" ]; then
+  # Sends message to Kris
+  curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"${message}\"}" https://hooks.slack.com/services/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
+else
+  echo "Format incorrect. ./test.sh \"Receiver\" \"Message\""
+fi
 
+# To curl a json file
+# curl -X POST -H 'Content-type: application/json' -d @text.json https://hooks.slack.com/services/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
